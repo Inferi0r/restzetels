aa6ea5e1f0238b76dc16f333085d9276ad761663
Arco Vink <arcovink@gmail.com>
2023-07-25 01:05:40 +0200

    bug in restzetels but good start

--- zetelverdeling.js
+++ zetelverdeling.js
@@ -18,24 +18,43 @@ const zetelverdelingHeaderRow = zetelverdelingTable.createTHead().insertRow();
 const zetelverdelingHeader1 = document.createElement('th');
 const zetelverdelingHeader2 = document.createElement('th');
 const zetelverdelingHeader3 = document.createElement('th');
+const zetelverdelingHeader4 = document.createElement('th');
 zetelverdelingHeader1.textContent = "Lijst";
 zetelverdelingHeader2.textContent = "Partij";
 zetelverdelingHeader3.textContent = "Volle zetels met kiesdeler";
+zetelverdelingHeader4.textContent = "Restzetels";
 zetelverdelingHeaderRow.appendChild(zetelverdelingHeader1);
 zetelverdelingHeaderRow.appendChild(zetelverdelingHeader2);
 zetelverdelingHeaderRow.appendChild(zetelverdelingHeader3);
+zetelverdelingHeaderRow.appendChild(zetelverdelingHeader4);
+
+// Store restzetels data for later use
+let restzetelsMap = new Map();
+let highestRestzetelsIndex = null;
+let highestRestzetelsValue = 0;
 
 // Populate table for zetelverdeling with data and calculate total seats
 let totalSeats = 0;
 votesData.parties.forEach((party, index) => {
-    const seats = Math.floor(parseInt(party.results.current.votes) / kiesdeler);
+    const votes = parseInt(party.results.current.votes);
+    const seats = Math.floor(votes / kiesdeler);
+    const average1 = Math.round(votes / (seats + 1));
+    restzetelsMap.set(index, average1);
+
+    if (average1 > highestRestzetelsValue) {
+        highestRestzetelsValue = average1;
+        highestRestzetelsIndex = index;
+    }
+
     const row = zetelverdelingTbody.insertRow();
     const listCell = row.insertCell();
     const partyCell = row.insertCell();
     const seatsCell = row.insertCell();
+    const restzetelsCell = row.insertCell();
     listCell.textContent = index + 1;
     partyCell.textContent = keyToLabel.get(party.key);
     seatsCell.textContent = seats;
+    restzetelsCell.textContent = index === highestRestzetelsIndex ? 1 : 0;
     totalSeats += seats;
 });
 
@@ -44,52 +63,58 @@ const zetelverdelingContainer = document.createElement('div');
 zetelverdelingContainer.style.marginTop = '20px'; // Add margin to top of container
 zetelverdelingContainer.appendChild(zetelverdelingTable);
 
-// Create Restzetels table and populate with data
-const restzetelsTable = document.createElement('table');
-const restzetelsTbody = restzetelsTable.createTBody();
-const restzetelsRow = restzetelsTbody.insertRow();
-const restzetelsLabelCell = restzetelsRow.insertCell();
-const restzetelsCell = restzetelsRow.insertCell();
-restzetelsLabelCell.textContent = "Restzetels:";
-restzetelsCell.textContent = (150 - totalSeats).toLocaleString('nl-NL');
+// Create Restzetels total table and populate with data
+const restzetelstotalTable = document.createElement('table');
+const restzetelstotalTbody = restzetelstotalTable.createTBody();
+const restzetelstotalRow = restzetelstotalTbody.insertRow();
+const restzetelstotalLabelCell = restzetelstotalRow.insertCell();
+const restzetelstotalCell = restzetelstotalRow.insertCell();
+restzetelstotalLabelCell.textContent = "Restzetels:";
+restzetelstotalCell.textContent = (150 - totalSeats).toLocaleString('nl-NL');
 
-// Create container for Restzetels table 
-const restzetelsContainer = document.createElement('div');
-restzetelsContainer.style.marginTop = '20px'; // Add margin to top of container
-restzetelsContainer.appendChild(restzetelsTable);
+// Create container for Restzetels total table 
+const restzetelstotalContainer = document.createElement('div');
+restzetelstotalContainer.style.marginTop = '20px'; // Add margin to top of container
+restzetelstotalContainer.appendChild(restzetelstotalTable);
 
-// Create calculation table
+// Create table for calculation data
 const calculationDataTable = document.createElement('table');
 const calculationDataTbody = calculationDataTable.createTBody();
 const calculationDataHeaderRow = calculationDataTable.createTHead().insertRow();
 const calculationDataHeader1 = document.createElement('th');
 const calculationDataHeader2 = document.createElement('th');
+const calculationDataHeader3 = document.createElement('th');
 calculationDataHeader1.textContent = "Lijst";
 calculationDataHeader2.textContent = "Partij";
+calculationDataHeader3.textContent = "Stemgemiddelde 1e restzetel";
 calculationDataHeaderRow.appendChild(calculationDataHeader1);
 calculationDataHeaderRow.appendChild(calculationDataHeader2);
+calculationDataHeaderRow.appendChild(calculationDataHeader3);
 
 // Populate table with calculation data
 votesData.parties.forEach((party, index) => {
-    const seats = Math.floor(parseInt(party.results.current.votes) / kiesdeler);
+    const votes = parseInt(party.results.current.votes);
+    const seats = Math.floor(votes / kiesdeler);
     if (seats > 0) {
         const row = calculationDataTbody.insertRow();
         const listCell = row.insertCell();
         const partyCell = row.insertCell();
+        const average1Cell = row.insertCell(); 
         listCell.textContent = index + 1;
         partyCell.textContent = keyToLabel.get(party.key);
+        average1Cell.textContent = Math.round(votes / (seats + 1)).toLocaleString('nl-NL'); 
     }
 });
-
+  
 // Create container for calculation data table
 const calculationDataContainer = document.createElement('div');
 calculationDataContainer.style.marginTop = '20px'; // Add margin to top of container
 calculationDataContainer.appendChild(calculationDataTable);
 
-// Append 3 tables to containers
+// Append 3 tables to 3 containers
 const tableContainer = document.getElementById('tableContainer');
 tableContainer.appendChild(zetelverdelingContainer);
-tableContainer.appendChild(restzetelsContainer);
+tableContainer.appendChild(restzetelstotalContainer);
 tableContainer.appendChild(calculationDataContainer);
 }
 
