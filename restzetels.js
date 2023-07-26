66e8e032120925f810c0d55f3456cdd76420967e
Arco Vink <arcovink@gmail.com>
2023-07-26 12:23:18 +0200

    revert to backup with wrong lijst numbers

--- restzetels.js
+++ restzetels.js
@@ -11,32 +11,44 @@ fetch('get_data.php?source=last_update')
     .then(response => response.json())
     .then(data => {
         votesData = data;
-        computeSeats();
         createVoteAverageTable();
         createRestSeatsTable();
     });
 
-function computeSeats() {
-    const totalVotes = votesData.parties.reduce((sum, party) => sum + parseInt(party.results.current.votes), 0);
-    const kiesdeler = Math.floor(totalVotes / 150);
+function createVoteAverageTable() {
+    let tableData = [];
 
+    let totalVotes = 0;
     votesData.parties.forEach(party => {
-        party.fullSeats = Math.floor(party.results.current.votes / kiesdeler);
-        party.restSeats = new Map();
+        totalVotes += parseInt(party.results.current.votes);
     });
 
-    total_restSeats = 150 - votesData.parties.reduce((sum, party) => sum + party.fullSeats, 0);
+    let kiesdeler = Math.floor(totalVotes / 150);
+
+    votesData.parties.forEach(party => {
+        let fullSeats = Math.floor(party.results.current.votes / kiesdeler);
+        party.fullSeats = fullSeats; 
+        party.restSeats = new Map(); 
+        if(fullSeats > 0) {
+            tableData.push({
+                'Lijst': party.key,
+                'Partij': keyToLabel.get(party.key)
+            });
+        }
+    });
+
+    let total_fullSeats = votesData.parties.reduce((acc, party) => acc + party.fullSeats, 0);
+    total_restSeats = 150 - total_fullSeats;
 
     for (let i = 1; i <= total_restSeats; i++) {
         let maxVoteAverage = 0;
         let partyWithMaxVoteAverage = null;
 
         votesData.parties.forEach(party => {
-            if (party.fullSeats > 0) {
+            if(party.fullSeats > 0) {
                 let restSeatsCount = Array.from(party.restSeats.values()).reduce((a, b) => a + b, 0);
                 let voteAverage = Math.round(party.results.current.votes / (party.fullSeats + restSeatsCount + 1));
-
-                if (voteAverage > maxVoteAverage) {
+                if(voteAverage > maxVoteAverage) {
                     maxVoteAverage = voteAverage;
                     partyWithMaxVoteAverage = party;
                 }
@@ -47,63 +59,80 @@ function computeSeats() {
             partyWithMaxVoteAverage.restSeats.set(i, 1);
         }
     }
-}
 
-function createVoteAverageTable() {
-    let tableData = votesData.parties.filter(party => party.fullSeats > 0).map(party => {
-        let rowData = {
-            'Lijst': party.key,
-            'Partij': keyToLabel.get(party.key)
-        };
+    tableData.forEach(rowData => {
+        let party = votesData.parties.find(p => p.key === rowData.Lijst);
         for (let i = 1; i <= total_restSeats; i++) {
             let restSeatsCount = Array.from(party.restSeats.values()).reduce((a, b) => a + b, 0);
-            rowData[`Stemgemiddelde ${i}e restzetel`] = Math.round(party.results.current.votes / (party.fullSeats + restSeatsCount));
+            rowData[`Stemgemiddelde voor ${i}e restzetel`] = Math.round(party.results.current.votes / (party.fullSeats + restSeatsCount + 1));
         }
-        return rowData;
     });
 
-    let headers = ['Lijst', 'Partij', ...Array.from({length: total_restSeats}, (_, i) => `Stemgemiddelde ${i+1}e restzetel`)];
-    let voteAverageTable = createHTMLTable(headers, tableData);
-    document.getElementById('voteAverageContainer').appendChild(voteAverageTable);
-}
-
-function createRestSeatsTable() {
-    let restSeatsTableData = Array.from({length: total_restSeats}, (_, i) => {
-        let party = votesData.parties.find(p => p.restSeats.get(i+1) === 1);
-        return party ? { 'Restzetel': i+1, 'Partij': keyToLabel.get(party.key) } : {};
-    });
-
-    let headers = ['Restzetel', 'Partij'];
-    let restSeatTable = createHTMLTable(headers, restSeatsTableData);
-    document.getElementById('restSeatContainer').innerHTML = '';
-    document.getElementById('restSeatContainer').appendChild(restSeatTable);
-}
-
-function createHTMLTable(headers, data) {
     let table = document.createElement('table');
     let thead = document.createElement('thead');
     let headerRow = document.createElement('tr');
-
-    headers.forEach(headerText => {
+    ['Lijst', 'Partij'].concat(
+        Array.from({length: total_restSeats}, (_, i) => `Stemgemiddelde voor ${i+1}e restzetel`)
+    ).forEach(headerText => {
         let header = document.createElement('th');
         header.appendChild(document.createTextNode(headerText));
         headerRow.appendChild(header);
     });
-
     thead.appendChild(headerRow);
     table.appendChild(thead);
 
     let tbody = document.createElement('tbody');
-    data.forEach(rowData => {
+    tableData.forEach(rowData => {
         let row = document.createElement('tr');
-        Object.entries(rowData).forEach(([key, value]) => {
+        Object.keys(rowData).forEach(key => {
+            if (!key.startsWith('Restzetel')) {
+                let cell = document.createElement('td');
+                cell.appendChild(document.createTextNode(rowData[key]));
+                row.appendChild(cell);
+            }
+        });
+        tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
+
+    document.getElementById('voteAverageContainer').appendChild(table);
+}
+
+function createRestSeatsTable() {
+    let restSeatsTableData = [];
+    for (let i = 1; i <= total_restSeats; i++) {
+        let party = votesData.parties.find(p => p.restSeats.get(i) === 1);
+        if (party) {
+            restSeatsTableData.push({
+                'Restzetel': i,
+                'Partij': keyToLabel.get(party.key),
+            });
+        }
+    }
+
+    let table = document.createElement('table');
+    let thead = document.createElement('thead');
+    let headerRow = document.createElement('tr');
+    ['Restzetel', 'Partij'].forEach(headerText => {
+        let header = document.createElement('th');
+        header.appendChild(document.createTextNode(headerText));
+        headerRow.appendChild(header);
+    });
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    let tbody = document.createElement('tbody');
+    restSeatsTableData.forEach(rowData => {
+        let row = document.createElement('tr');
+        Object.values(rowData).forEach(cellData => {
             let cell = document.createElement('td');
-            cell.appendChild(document.createTextNode(value));
+            cell.appendChild(document.createTextNode(cellData));
             row.appendChild(cell);
         });
         tbody.appendChild(row);
     });
-
     table.appendChild(tbody);
-    return table;
+
+    document.getElementById('restSeatContainer').innerHTML = '';
+    document.getElementById('restSeatContainer').appendChild(table);
 }
